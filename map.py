e20450af15d15c127cc4d72b5482979a863e2982
orsoneveraert <74539165+orsoneveraert@users.noreply.github.com>
2024-08-17 10:19:04 +0200

    Update map.py

--- map.py
+++ map.py
@@ -6,7 +6,9 @@ from urllib.parse import quote_plus
 def init_connection():
     username = quote_plus(st.secrets["mongo"]["username"])
     password = quote_plus(st.secrets["mongo"]["password"])
-    cluster = st.secrets["mongo"]["cluster"]
+    host = st.secrets["mongo"]["host"]
+    # Extract the cluster name from the host
+    cluster = '.'.join(host.split('.')[-3:])
     connection_string = f"mongodb+srv://{username}:{password}@{cluster}/?retryWrites=true&w=majority"
     return pymongo.MongoClient(connection_string)
 
@@ -14,7 +16,7 @@ client = init_connection()
 
 @st.cache_data(ttl=600)
 def get_data():
-    db = client[st.secrets["mongo"]["database"]]
+    db = client['mazette']  # Replace 'mazette' with your actual database name if different
     items = db.mycollection.find()
     items = list(items)  # make hashable for st.cache_data
     return items
@@ -26,5 +28,6 @@ for item in items:
     st.write(f"{item['name']} has a :{item['pet']}:")
 
 # Optionally, you can print the connection string (without password) to verify it's correct
-connection_string = f"mongodb+srv://{st.secrets['mongo']['username']}:****@{st.secrets['mongo']['cluster']}/?retryWrites=true&w=majority"
+cluster = '.'.join(st.secrets['mongo']['host'].split('.')[-3:])
+connection_string = f"mongodb+srv://{st.secrets['mongo']['username']}:****@{cluster}/?retryWrites=true&w=majority"
 st.write(f"Connecting to: {connection_string}")
